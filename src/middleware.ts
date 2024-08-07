8d50a4a791c19816c895e85c7cf7ecf6bd9da947
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-08-07 13:37:40 +0200

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,8 @@ export async function middleware(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
   const isAuthPath = req.nextUrl.pathname.startsWith('/auth');
 
-  // Allow requests for static files, API routes, and Next.js internal requests to pass through
+  console.log(`Token: ${token}, Path: ${req.nextUrl.pathname}`);
+
   if (
     req.nextUrl.pathname.startsWith('/_next/') ||
     req.nextUrl.pathname.startsWith('/api/') ||
@@ -18,19 +19,15 @@ export async function middleware(req: NextRequest) {
     return NextResponse.next();
   }
 
-  // If there is no token and the request is not for an authentication path, redirect to sign-in
   if (!token && !isAuthPath) {
     const signInUrl = new URL('/auth/signin', req.url);
-    signInUrl.searchParams.set('callbackUrl', req.nextUrl.pathname);
     return NextResponse.redirect(signInUrl);
   }
 
-  // If there is a token and the request is for an authentication path, redirect to home
   if (token && isAuthPath) {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
-  // Allow the request to proceed
   return NextResponse.next();
 }
 
