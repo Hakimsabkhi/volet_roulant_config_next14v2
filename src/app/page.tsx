0cf8d7f3adce133cf8bce7f93d57b615bd6900ab
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-08-16 13:22:52 +0200

    optimise loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,13 @@
-"use client";
+import React from "react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions"; // adjust the import path to where your authOptions are defined
 
-import React, { useState, useEffect } from "react";
-import { useSession } from "next-auth/react";
-import LoadingSpinner from "@/components/LoadingSpinner";
-
-const Home: React.FC = () => {
-  const { data: session, status } = useSession();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    if (status !== "loading") {
-      setLoading(false);
-    }
-  }, [status]);
-
-  if (loading) {
-    return <LoadingSpinner />;
-  }
+const Home = async () => {
+  const session = await getServerSession(authOptions);
 
   return (
     <main className="flex gap-4 justify-between w-[90%]">
-      <div className="flex flex-col gap-10 ">
+      <div className="flex flex-col gap-10">
         <div className="flex flex-col">
           <h1>Bienvenue, {session?.user?.name || "Utilisateur"}!</h1>
           <p>Ceci est votre page d&apos;accueil.</p>
