a66a929de2d18499ea73edc9893607563eff5208
Hakimsabkhi <hakimsabkhi001@gmail.com>
2024-09-02 10:57:18 +0200

    fix auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
-import { getServerSession } from "next-auth/next";
+import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/lib/authOptions';
 import Providers from '../components/Providers';
 import ClientLayout from '@/components/ClientLayout'; 
 import SessionProviderWrapper from '../components/SessionProviderWrapper';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import React, { useMemo } from 'react';
+import React from 'react';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '700'] });
 
@@ -18,23 +18,13 @@ export const metadata: Metadata = {
 };
 
 const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+  // Fetch the session data
   const session = await getServerSession(authOptions);
-
-  return (
-    <RootLayoutClient session={session}>
-      {children}
-    </RootLayoutClient>
-  );
-}
-
-const RootLayoutClient: React.FC<{ session: any, children: React.ReactNode }> = ({ session, children }) => {
-  // Memoize the session data to avoid unnecessary recomputation
-  const memoizedSession = useMemo(() => session, [session]);
 
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <SessionProviderWrapper session={memoizedSession}>
+        <SessionProviderWrapper session={session}>
           <Providers>
             <ClientLayout>
               <ToastContainer 
